55d01f8b5400e942c4ed2f3a6d4efab9279bc19a
Michael Beebe <beebe79@gmail.com>
2021-02-19 14:47:23 -0500

    simplified HTML, set click for timer, added function for Q&A

--- hm4.js
+++ hm4.js
@@ -1,9 +1,79 @@
- var Ans1 = document.querySelector("#answer1");
-var Ans2 = document.querySelector("#answer2");
-var Ans3 = document.querySelector("#answer3");
-var Ans4 = document.querySelector("#answer4");
-var Questions = document.querySelector("#questions");
-var HighScores = document.querySelector("#Highscores");
-var Timer = document.querySelector("#timer");
-var startBtn = document.querySelector("#Start");
- var clearBtn = document.querySelector("#Clear");
+
+var  quizQuestions   = [
+     {
+         question: 'Commonly used data types DO NOT include:',
+         choices: ["strings","booleans",'alerts', 'numbers'],
+         answer: 'alerts'
+     },
+     {
+         question: 'The condition in an if / else statement is enclosed within_______.',
+         choices: ['quotes','curly brackets','parentheses','square brackets'],
+         answer: 'parentheses'
+     },
+     {
+         question: 'Arrays in Javascript can be used to store _______.',
+         choices: ['numbers and strings','other arrays','booleans','all of the above'],
+         answer: 'all of the above'
+     },
+     {
+         question: 'String values must be enclosed within _____ when being assigned to variables',
+         choices: ['commas','curly brackets','quotes','parentheses'],
+         answer: 'quotes'
+     },
+     {
+         question: 'A very useful tool used during development and debugging for printing content to the debugger is',
+         choices: ['Javascript','terminal/bash','for loops','console log'],
+         answer: 'console log'
+     },
+ ];
+
+ var score = 0;
+ var questionIndex = 0;
+ var currentTime = document.querySelector('#currentTime');
+ var questions = document.querySelector('#questions');
+ var timer = document.querySelector('#Start');
+ var content = document.querySelector('#content');
+ var secondsLeft = 75;
+ var holdInterval = 0;
+ var penaltyTime = 15;
+ var createUl = document.createElement('ul');
+
+
+ timer.addEventListener("click", function() {
+     if(holdInterval === 0) {
+         holdInterval = setInterval(function() { secondsLeft--;
+        currentTime.textContent = "Timer:   " + secondsLeft;
+    
+    if(secondsLeft <= 0){ clearInterval(holdInterval);
+    finished();
+currentTime.textContent = "Time is finished!";}
+}, 1000);
+     } begin(questionIndex);
+ });
+
+
+function begin(questionIndex) {
+    questions.innerHTML ="";
+    createUl.innerHTML ="";
+
+    for (var i = 0; i<quizQuestions.length; i++) {
+        var userQuestion = quizQuestions[questionIndex].question;
+        var userChoices = quizQuestions[questionIndex].choices;
+        questions.textContent = userQuestion;
+    }
+
+    userChoices.forEach(function (newItem) { varlistItem = document.createElement('li');
+listItem.textContent = newItem;
+questions.appendChild(createUl);
+createUl.appendChild(listItem);
+listItem.addEventListener('click',(compare));})
+}
+
+function compare(event) {
+    var element = event.target;
+
+    if (element.matches('li')) {
+        var createDiv = document.createElement('div');
+        createDiv.setAttribute('id', 'createDiv');
+    }
+}
